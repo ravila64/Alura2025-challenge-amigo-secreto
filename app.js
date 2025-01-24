b6ca70b001ddc98461c629c12807e5b84bf7e5ee
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-01-24 16:57:58 -0500

    Inicia array listadeAmigos y agregarAmigos()

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Variables globales
+let listaAmigosSorteados=[];
+
+
+// funcion agregar amigos
+function agregarAmigo(){
+    // obtener el contenido de la captura en html
+    const textoEntrada = document.getElementById('amigo');
+    const nombreAmigo = (textoEntrada.value).trim();
+    if (nombreAmigo===null || nombreAmigo===''){
+        alert("Por favor colocar nombres validos, sin  blancos ");
+    }
+    console.log("Amigo digitado ", nombreAmigo, "long:",nombreAmigo.length);
+    listaAmigosSorteados.push(nombreAmigo);
+    // Limpia campo del input
+    nombreAmigo.value = "";
+}
+agregarAmigo();
+
+// actualizar lista de amigos
+function listarAmigos(){
+    
+}
