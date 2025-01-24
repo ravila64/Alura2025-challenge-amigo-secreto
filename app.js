bd83cc42f23afedc238f0215c872de3d6d420bc4
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2025-01-24 18:39:09 -0500

    agregar listaAmigos, camelCase, validar repetidos

--- app.js
+++ app.js
@@ -9,18 +9,66 @@ let listaAmigosSorteados=[];
 function agregarAmigo(){
     // obtener el contenido de la captura en html
     const textoEntrada = document.getElementById('amigo');
-    const nombreAmigo = (textoEntrada.value).trim();
-    if (nombreAmigo===null || nombreAmigo===''){
-        alert("Por favor colocar nombres validos, sin  blancos ");
+    // si esta vacio o null el nombre a añadir
+    let nombreAmigo = (textoEntrada.value).trim();
+    console.log("Nombre amigo ", nombreAmigo);
+    if (nombreAmigo===null || nombreAmigo==='') {
+        alert("Por favor colocar nombres validos sin  blancos ");
+    }
+    nombreAmigo = camelCase(nombreAmigo);
+    // validar si existe amigo
+    if (validaRepetidos(nombreAmigo)){
+        alert(`Nombre ${nombreAmigo},  ya existe !!!, Inserte otro nombre`);
+        return;
     }
     console.log("Amigo digitado ", nombreAmigo, "long:",nombreAmigo.length);
     listaAmigosSorteados.push(nombreAmigo);
     // Limpia campo del input
-    nombreAmigo.value = "";
+    textoEntrada.value = "";
+    listarAmigos();
 }
+
 agregarAmigo();
 
 // actualizar lista de amigos
 function listarAmigos(){
-    
+    limpiarLista();
+    for (let i = 0; i < listaAmigosSorteados.length; i++) {
+        const nombre = listaAmigosSorteados[i]; 
+        console.log(nombre);        
+        // Crea un nuevo elemento <li>
+        const nuevoAmigo = document.createElement('li');
+        nuevoAmigo.textContent = nombre; // Establece el texto del <li>
+        // Agrega el <li> a la lista <ul>
+        const listaAmigos = document.getElementById('listaAmigos');
+        listaAmigos.appendChild(nuevoAmigo);
+    }
 }
+
+// limpiar lista UL
+function limpiarLista(){
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ""; // Limpia cualquier contenido existente
+}
+
+// desafios extra
+// 1. En los nombres que se capturan Colocarlos en “Camel Case”
+function camelCase(nombre){
+    let name = nombre.toLowerCase();
+    let caracter = nombre.charAt(0).toUpperCase();
+    let str=name.substring(1,name.length);
+    name=caracter.concat(str);
+    console.log("Nombre camelCase ",name);
+    return name;
+}
+
+// 2. Revisar que no estén repetidos los nombres
+function validaRepetidos(nombre){
+    if (listaAmigosSorteados.includes(nombre)) {
+        return true;       
+    }
+    return false;
+}
+
+// funcion para sortear amigos
+let numeroAmigos = listaAmigosSorteados.length;
